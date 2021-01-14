7308b1594e8efce06eb55f86fa8383208a111224
Soren Rood <sorenrood@gmail.com>
2021-01-14 04:40:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+from PIL import Image
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+print(pytesseract.image_to_string(Image.open('spu.jpg')))
