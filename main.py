5f7d10c31aefdf98456e86deb88bddf2951d462a
Soren Rood <sorenrood@gmail.com>
2021-01-15 00:49:15 -0800

    It's kinda working!

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from PIL import Image
 import pytesseract
 import enchant
 import nltk
+import os
+from azure.ai.textanalytics import TextAnalyticsClient
+from azure.core.credentials import AzureKeyCredential
 
 # Get text from image
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
@@ -14,13 +17,26 @@ d = enchant.Dict("en_US")
 words = []
 
 # Clean Data
-bad_chars = [';', ':', '!', "*", "|", "{", "}"]
+bad_chars = [';', ':', '!', "*", "|", "{", "}", "\n", "\t"]
 cleaned = ''.join((filter(lambda i: i not in bad_chars, text)))
-print(cleaned)
 
 # Tokenizing (convert into sentences)
 sentence_list = nltk.tokenize.sent_tokenize(cleaned)
+
+# Create document to send to azure
+documents = []
+i = 1
 for sentence in sentence_list:
-    print('-----------------')
-    print(sentence)
-    print('-----------------')
+    documents.append({"id": i, "language": "en", "text": sentence})
+    i += 1
+
+# Create a client
+key = os.getenv('AZURE_API_KEY')
+endpoint = 'https://soren-sentiment.cognitiveservices.azure.com/'
+credential = AzureKeyCredential(key)
+client = TextAnalyticsClient(endpoint=endpoint, credential=credential)
+
+# Get sentiment
+resp = client.analyze_sentiment(documents=documents)
+print(resp)
+    
