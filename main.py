7ea5b0b4c23174bdd280d26bb9781ed07e490b05
Soren Rood <sorenrood@gmail.com>
2021-01-14 17:08:22 -0800

    Added logic to remove words that are not in the english dictionary.

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
+# https://www.digitalocean.com/community/tutorials/how-to-perform-sentiment-analysis-in-python-3-using-the-natural-language-toolkit-nltk
+
 from PIL import Image
 import pytesseract
+import enchant
+
+# Get text from image
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-print(pytesseract.image_to_string(Image.open('pic.jpg')))
+text = pytesseract.image_to_string(Image.open('pic.jpg'))
+
+# Make dictionary checker and list to add true words to
+d = enchant.Dict("en_US")
+words = []
+
+# Split all words, check words, append to words if real word
+for word in text.split(' '):
+    if d.check(word):
+        words.append(word)
+    else:
+        continue
+
+for word in words:
+    print(word)
