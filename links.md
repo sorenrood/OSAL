3c310bf0be56709ac835a7c4865d215ace2f3705
Soren Rood <sorenrood@gmail.com>
2021-01-15 00:19:18 -0800

    Got sentiment analysis working.

---
+++ links.md
@@ -0,0 +1,3 @@
+# https://docs.microsoft.com/en-us/azure/cognitive-services/text-analytics/quickstarts/client-libraries-rest-api?tabs=version-3-1&pivots=programming-language-python
+
+# https://pypi.org/project/azure-ai-textanalytics/
--- sent.py
+++ sent.py
@@ -1,8 +1,20 @@
-# https://docs.microsoft.com/en-us/azure/cognitive-services/text-analytics/quickstarts/client-libraries-rest-api?tabs=version-3-1&pivots=programming-language-python
-# https://pypi.org/project/azure-ai-textanalytics/
 import os
 from azure.ai.textanalytics import TextAnalyticsClient
 from azure.core.credentials import AzureKeyCredential
+
+# Create a client
 key = os.getenv('AZURE_API_KEY')
 endpoint = 'https://soren-sentiment.cognitiveservices.azure.com/'
-print(key)
+credential = AzureKeyCredential(key)
+client = TextAnalyticsClient(endpoint=endpoint, credential=credential)
+
+# Define documents
+documents = [
+    {"id": "1", "language": "en", "text": "I hated the movie. It was so slow!"},
+    {"id": "2", "language": "en", "text": "The movie made it into my top ten favorites."},
+    {"id": "3", "language": "en", "text": "What a great movie!"}
+]
+
+# Get sentiment
+resp = client.analyze_sentiment(documents=documents)
+print(resp)
