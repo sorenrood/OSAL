b3bca8066674c5e248e623778817d740aaf1f421
Soren Rood <sorenrood@gmail.com>
2021-01-14 17:59:45 -0800

    fixing bugs.

--- azure.py
+++ azure.py
@@ -1,23 +1,6 @@
 # https://docs.microsoft.com/en-us/azure/cognitive-services/text-analytics/quickstarts/client-libraries-rest-api?tabs=version-3-1&pivots=programming-language-python
+# https://pypi.org/project/azure-ai-textanalytics/
 import os
-from azure.ai.textanalytics import TextAnalyticsClient
 from azure.core.credentials import AzureKeyCredential
-
-def azure_sentiment(text, key):
-    import requests
-    documents = { 'documents': [
-            { 'id': '1', 'text': text }
-            ]}
-    
-    azure_key = key
-    azure_endpoint = 'https://soren-sentiment.cognitiveservices.azure.com/'
-    assert azure_key
-    sentiment_azure = azure_endpoint + '/sentiment'
-    
-    headers   = {"Ocp-Apim-Subscription-Key": azure_key}
-    response  = requests.post(sentiment_azure, headers=headers, json=documents)
-    sentiments = response.json()
-    return sentiments
-
+from azure.ai.textanalytics import TextAnalyticsClient
 key = os.getenv('AZURE_API_KEY')
-endpoint = "https://soren-sentiment.cognitiveservices.azure.com/"
