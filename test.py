599564039c4413174b42cd3b56284cf10e737d11
Soren Rood <sorenrood@gmail.com>
2021-01-15 00:03:28 -0800

    Don't name your file after the libary =)

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 # https://docs.microsoft.com/en-us/azure/cognitive-services/text-analytics/quickstarts/client-libraries-rest-api?tabs=version-3-1&pivots=programming-language-python
 # https://pypi.org/project/azure-ai-textanalytics/
 import os
-from azure.core.credentials import AzureKeyCredential
 from azure.ai.textanalytics import TextAnalyticsClient
+# from azure.ai.textanalytics import TextAnalyticsClient
+# from azure.core.credentials import AzureKeyCredential
 key = os.getenv('AZURE_API_KEY')
