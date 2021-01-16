2ce65fe71934170f82d8c0bade2321963fd68d6f
Soren Rood <sorenrood@gmail.com>
2021-01-16 10:52:09 -0800

    cleaning.

--- client_test.py
+++ client_test.py
@@ -1,6 +1,7 @@
 import os
 from client import Client
 
+# Define variables essential for the client to run
 azure_endpoint = 'https://soren-sentiment.cognitiveservices.azure.com/'
 azure_key = os.getenv('AZURE_API_KEY')
 tesseract_path = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
@@ -11,10 +12,5 @@ client = Client(azure_endpoint=azure_endpoint, azure_key=azure_key,
 
 client.image_to_string()
 client.clean_tesseract_resp()
-print(client.clean_text)
-
 client.text_to_document()
-print(client.documents)
-
 client.analyze_sentiment()
-
