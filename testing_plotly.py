7ebb6dc0a88e61d34ed1d7caa3c34da655e1ad2a
Soren Rood <sorenrood@gmail.com>
2021-01-25 23:13:43 -0800

    Removed some code.

--- testing_plotly.py
+++ testing_plotly.py
@@ -1,13 +1,6 @@
 import pandas as pd
 import plotly.express as px
 
-# data = {
-#     'positive': [1],
-#     'negative': [4],
-#     'neutral': [7],
-#     'date': ['jan-1']
-# }
-
 data = {
     'sentiscore': [4, 2, 6, 7, 3, 8, 5, 4],
     'date': ['jan-1', 'jan-2', 'jan-2', 'jan-3', 'jan-4', 'jan-5', 'jan-6', 'jan-7']
