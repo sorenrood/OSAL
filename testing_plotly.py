7e5d86d2492f805c35a557356b9a41e82090bf82
Soren Rood <sorenrood@gmail.com>
2021-01-25 22:53:44 -0800

    sentiscore

--- testing_plotly.py
+++ testing_plotly.py
@@ -9,8 +9,8 @@ import plotly.express as px
 # }
 
 data = {
-    'sentiscore': [4],
-    'date': ['jan-1']
+    'sentiscore': [4, 2, 6, 7, 3, 8, 5, 4],
+    'date': ['jan-1', 'jan-2', 'jan-2', 'jan-3', 'jan-4', 'jan-5', 'jan-6', 'jan-7']
 }
 
 df = pd.DataFrame(data, columns=['sentiscore', 'date'])
